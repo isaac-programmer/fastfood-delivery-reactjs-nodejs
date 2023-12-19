16668363e089a3808c7ec8612845e24d8426dc11
Isaac Daniel <isaacbusiness2019@hotmail.com>
2023-12-19 12:00:34 -0300

    feat: express no projeto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello Worl!");
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+let qtdInsertedUsers = 1;
+
+// Habilitando o CORS
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+// Middleware para parsear JSON do corpo da requisição
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`App escutando na porta ${port}`);
+});
